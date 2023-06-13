a5a9aadfbc9ad4121a635f57ecd5fdb1b8b0033f
Maryia Tsvilik <mtsvilik@solvd.com>
2023-06-13 17:24:35 +0300

    New lessons were added

---
+++ lesson_7/hangman_project/hangman.py
@@ -0,0 +1,36 @@
+import random
+from hangman_art import logo, stages
+from hangman_words import word_list
+
+end_of_game = False
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+lives = 6
+
+print(logo)
+
+display = []
+for letter in chosen_word:
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a latter: ").lower()
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win!")
+
+    print((stages[lives]))
