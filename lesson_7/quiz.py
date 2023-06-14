7f5da8de35d5962bd8e2dbbf3f7cc1788ff90327
Maryia Tsvilik <mtsvilik@solvd.com>
2023-06-14 16:26:35 +0300

    quiz.py was added

---
+++ lesson_7/quiz.py
@@ -0,0 +1,22 @@
+starting_dictionary = {
+    "a": 9,
+    "b": 8,
+}
+
+final_dictionary = {
+    "a": 9,
+    "b": 8,
+    "c": 7,
+}
+
+dict = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+}
+
+order = {
+    "starter": {1: "Salad", 2: "Soup"},
+    "main": {1: ["Burger", "Fries"], 2: ["Steak"]},
+    "dessert": {1: ["Ice Cream"], 2: []},
+}
